41d4ecdc3d8375c20dd20c2f6ada875bef4c3e1f
labroche <corentin.labroche@inist.fr>
2018-08-03 21:44:42 +0200

    [MINOR] CRUD on categories

--- src/app/category/categories/categories.component.ts
+++ src/app/category/categories/categories.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { CltPopupComponent, CltSidePanelComponent } from 'ngx-callisto/dist';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { GraphQLService } from '../../../graphQL/providers/graphQL.service';
+import { CommonService } from '../../providers/common.service';
 
 @Component({
   selector: 'categories',
@@ -6,10 +10,77 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./categories.component.scss']
 })
 export class CategoriesComponent implements OnInit {
+  categories = [];
+  addCategoryForm: FormGroup;
+  @ViewChild('addPopup') addPopup: CltPopupComponent;
+  @ViewChild('deletePopup') deletePopup: CltPopupComponent;
+  @ViewChild('actionMenu') actionMenu: CltSidePanelComponent;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder,
+    private graphql: GraphQLService,
+    private common: CommonService
+  ) { }
 
   ngOnInit() {
+    this.initForms()
+    setTimeout(() => this.common.routeName = 'Categories');
+    this.getCategories();
+  }
+  initForms() {
+    this.addCategoryForm = this.fb.group({
+      name: ['', Validators.required]
+    })
+  }
+
+  openActionMenu(event) {
+    this.actionMenu.title = event.name;
+    this.actionMenu.open(event);
+  }
+
+  async getCategories() {
+    const {categories} = await this.graphql.query(`categories { uuid, name }`)
+    if(!categories) return;
+    this.categories = this.common.merge(this.categories, categories);
+  }
+  addCategory() {
+    this.actionMenu.close();
+    this.addPopup.bindForm(this.addCategoryForm).open().subscribe(result=>{
+      if(!result) return;
+      this.graphql.mutation(`
+        categoryCreate(input: ${this.graphql.stringifyWithoutPropertiesQuote(result)}) {
+          uuid, name
+        }
+      `).then(_=>{
+        this.initForms()
+        return this.getCategories()
+      })
+    })
+  }
+
+  updateCategory(category) {
+    this.actionMenu.close();
+    this.addCategoryForm.patchValue(category);
+    this.addPopup.bindForm(this.addCategoryForm).open(category).subscribe(result => {
+      if (!result) return;
+      console.log(result)
+      this.graphql.mutation(`
+        categoryUpdate(uuid:"${category.uuid}", input:${this.graphql.stringifyWithoutPropertiesQuote(result)}) {
+          uuid
+        }
+      `).then(_ => {
+          return this.getCategories();
+        });
+    });
+  }
+  deleteCategory(item) {
+    this.actionMenu.close();
+    this.deletePopup.open().subscribe(result => {
+      if (!result) return;
+      return this.graphql.mutation(`
+        categoryDelete(uuid: "${item.uuid}")
+      `).then(_ => this.getCategories());
+    });
   }
 
 }
--- src/app/category/category.module.ts
+++ src/app/category/category.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { CategoriesComponent } from './categories/categories.component';
 import { RheaSdkModule } from '../rhea-sdk/rhea-sdk.module';
 import { RouterModule } from '@angular/router';
+import { CltOverlayModule } from 'ngx-callisto/dist';
 
 @NgModule({
   imports: [
     CommonModule,
     RheaSdkModule,
+    CltOverlayModule,
     RouterModule.forChild([
       { path: '', component: CategoriesComponent }
     ])
