311efdab783481dbb06fee1f602f71c91c99bc15
labroche <corentin.labroche@inist.fr>
2018-08-04 01:29:07 +0200

    [PATCH] sort shoppinglists

--- src/app/shopping-list/shopping-lists/shopping-lists.component.ts
+++ src/app/shopping-list/shopping-lists/shopping-lists.component.ts
@@ -3,7 +3,7 @@ import { GraphQLService } from '../../../graphQL/providers/graphQL.service';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { CltPopupComponent, CltSidePanelComponent } from 'ngx-callisto/dist';
 import { CommonService } from '../../providers/common.service';
-import { TouchSequence } from 'selenium-webdriver';
+import * as sort from 'fast-sort';
 
 @Component({
   selector: 'shopping-lists',
@@ -47,9 +47,33 @@ export class ShoppingListsComponent implements OnInit, OnDestroy {
     this.shoppingListForm.reset();
   }
   openActionMenu(event) {
-    const i = this.shoppingLists.indexOf(event);
     this.actionMenu.title = event.name;
     this.actionMenu.open(event);
+  }
+
+  getAllShoppingList() {
+    this.graphql.query(`
+      shoppingLists {
+        uuid
+        name
+        description
+        createdAt
+        items {
+          uuid
+          name
+          description
+        }
+      }
+    `).then(({ shoppingLists }) => {
+        shoppingLists = shoppingLists.map(shoppingList => {
+          shoppingList.createdAt = new Date(shoppingList.createdAt);
+          return shoppingList;
+        });
+        sort(shoppingLists).desc('createdAt')
+        if (this.shoppingLists.length > shoppingLists.length) this.shoppingLists = shoppingLists;
+        else this.shoppingLists = this.common.merge(this.shoppingLists, shoppingLists);
+        return this.shoppingLists;
+      });
   }
 
   updateItem(shoppingList) {
@@ -75,29 +99,7 @@ export class ShoppingListsComponent implements OnInit, OnDestroy {
       `).then(_ => this.getAllShoppingList());
     });
   }
-  getAllShoppingList() {
-    this.graphql.query(`
-      shoppingLists {
-        uuid
-        name
-        description
-        createdAt
-        items {
-          uuid
-          name
-          description
-        }
-      }
-    `).then(({ shoppingLists }) => {
-      shoppingLists = shoppingLists.map(shoppingList => {
-        shoppingList.createdAt = new Date(shoppingList.createdAt);
-        return shoppingList;
-      });
-      if(this.shoppingLists.length > shoppingLists.length) this.shoppingLists = shoppingLists;
-      else this.shoppingLists = this.common.merge(this.shoppingLists, shoppingLists);
-      return this.shoppingLists;
-    });
-  }
+  
   addShoppingList() {
     return this.addPopup.bindForm(this.shoppingListForm).open().subscribe(async result => {
       this.resetForms();
