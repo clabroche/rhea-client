2f4b0af4f847d2b4985817b76a328c2389224e48
labroche <corentin.labroche@inist.fr>
2018-08-03 19:13:06 +0200

    [MINOR] View all items and can delete

--- src/app/item/item.module.ts
+++ src/app/item/item.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ItemComponent } from './item/item.component';
 import { ItemsComponent } from './items/items.component';
+import { RouterModule } from '@angular/router';
+import { RheaSdkModule } from '../rhea-sdk/rhea-sdk.module';
+import { CltOverlayModule } from 'ngx-callisto/dist';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RheaSdkModule,
+    CltOverlayModule,
+    RouterModule.forChild([
+      {path:'items', component: ItemsComponent}
+    ])
   ],
   declarations: [ItemComponent, ItemsComponent]
 })
--- src/app/item/items/items.component.ts
+++ src/app/item/items/items.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { GraphQLService } from '../../../graphQL/providers/graphQL.service';
+import { CommonService } from '../../providers/common.service';
+import { CltPopupComponent } from 'ngx-callisto/dist';
 
 @Component({
   selector: 'items',
@@ -6,10 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./items.component.scss']
 })
 export class ItemsComponent implements OnInit {
-
-  constructor() { }
+  items = [];
+  @ViewChild('deletePopup') deletePopup: CltPopupComponent;
+  
+  constructor(
+    private graphql: GraphQLService,
+    private common: CommonService
+  ) { }
 
   ngOnInit() {
+    setTimeout(() => this.common.routeName = 'Items');
+    setInterval(() => {
+      this.getAllItems();
+    }, this.common.refreshInterval);
+    this.getAllItems();
+  }
+ 
+  async getAllItems () {
+    const {items} = await this.graphql.query(`items { uuid, name, description }`);
+    if(!items) return;
+    this.items = this.common.merge(this.items, items);
   }
 
+  async deleteItem(uuid) {
+    this.deletePopup.open().subscribe(result=>{
+      if(!result) return;
+      this.graphql.mutation(`itemDelete(uuid: "${uuid}")`)
+        .then(_=>this.getAllItems())
+    })
+  }
 }
